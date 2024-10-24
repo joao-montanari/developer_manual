12a65415ea26ea2f685dd2c5b2ba73e259491828
Silva Joao (BISB/PJ-DF) <joao.silva7@br.bosch.com>
2024-10-24 17:06:32 -0300

    atividade - modulo 2

--- Cursos/Lógica de Programação - Mergulhe em Programação com JavaScript/Apresentação.md
+++ Cursos/Lógica de Programação - Mergulhe em Programação com JavaScript/Apresentação.md
@@ -1,7 +1,7 @@
 Nesse curso vamos aprender o motivo pelo qual a Lógica de Programação é importante para nossa carreira como **pessoas desenvolvedoras** de software. Conheceremos conceitos comuns para a maioria das linguagens.
 No desenrolar do curso vai ser desenvolvido um jogo de número secreto, na onde o usuário vai ter que adivinhar o número aleatório sorteado pelo programa pro meio de dicas.
 
-### Documentação e guias
+## Documentação e guias
 - https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/First_steps/What_is_JavaScript
 - https://www.alura.com.br/artigos/javascript
 
--- Pratica - Projetos Base/Lógica de Programação - Mergulhe em Programação com JavaScript/logica-js-aula_2/app.js
+++ Pratica - Projetos Base/Lógica de Programação - Mergulhe em Programação com JavaScript/logica-js-aula_2/app.js
@@ -3,9 +3,15 @@ let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+while (chute != numeroSecreto) {
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+    }
 }
